79fd898bb116d1f8af4dffaae91685b0e1f63246
Lou DeGenaro <lou.degenaro@gmail.com>
2025-03-25 07:51:41 -0400

    data-driven scripting
    Signed-off-by: Lou DeGenaro <lou.degenaro@gmail.com>

--- scripts/automation/update_profile.sh
+++ scripts/automation/update_profile.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 export COMMIT_TITLE="chore: Catalogs automatic update."
-export COMMIT_BODY="Sync catalogs with ocp-oscal-catalogs repo"
+export COMMIT_BODY="Sync catalogs with catalogs repo"
 git config --global user.email "automation@example.com"
 git config --global user.name "AutomationBot" 
-cd e2e-demo-profile
+cd $(python automation.py "update-profile-folder")
 git checkout -b "catalogs_autoupdate_$GITHUB_RUN_ID"
 cp -r ../catalogs .
 if [ -z "$(git status --porcelain)" ]; then 
@@ -15,7 +15,7 @@ else
      echo "Nothing to commit" 
   else
      git commit -m "$COMMIT_TITLE"
-     remote=https://x-access-token:$GH_TOKEN@github.com/oscal-compass/e2e-demo-profile
+     remote=$(python automation.py "update-profile-remote")
      git push -u "$remote" "catalogs_autoupdate_$GITHUB_RUN_ID"
      echo $COMMIT_BODY
      gh pr create -t "$COMMIT_TITLE" -b "$COMMIT_BODY" -B "develop" -H "catalogs_autoupdate_$GITHUB_RUN_ID" 
